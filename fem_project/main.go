63e8246d7d4f530db8d53e51ce35384505d9e44f
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2025-03-13 19:32:31 +0530

    added chi-library

---
+++ fem_project/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/rakim0/femProject/internal/app"
+	"github.com/rakim0/femProject/internal/routes"
+)
+
+func main() {
+	app, err := app.NewApplication()
+
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
+	if err != nil {
+		panic(err)
+	}
+	r := routes.SetupRoutes(app)
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		Handler:      r,
+	}
+	app.Logger.Printf("We are running on port %d", port)
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
